c5d972abb47253066564ecd57a831ef17de9c1ec
Herman Dmitriev <geronioplay.exzplay@gmail.com>
2026-06-02 23:57:54 +0500

    chore: fix version fix: add import SessionDep in api/comment.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.api.router import api_router
 from app.core.config import settings
 
 app = FastAPI(
-    title=settings.PROJECT_NAME, description="Простой Blog API", version="0.1.0"
+    title=settings.PROJECT_NAME, description="Простой Blog API", version="0.1.2"
 )
 
 app.include_router(api_router)
